9a244ded6001474357a3ee7c7a18c999f3fad424
Paul Perdomo <pperdomo@magnoliaisd.org>
2023-04-14 07:14:18 -0500

    Fix Test issue

--- Test.java
+++ Test.java
@@ -17,36 +17,36 @@ public class Test {
         int [][]  arr5 = {{1,1,1},{3,1,-2},{2,-2,1}};
 
         
-        assert Ans.mean(arr1) == 5;
-        assert Ans.mean(arr2) == 5.25;
-        assert Ans.max(arr1) == 9;
-        assert Ans.max(arr3) == -1;
+        assert Problems.mean(arr1) == 5;
+        assert Problems.mean(arr2) == 5.25;
+        assert Problems.max(arr1) == 9;
+        assert Problems.max(arr3) == -1;
         
         int[] col1 = {2,5,8};
-        assert Arrays.equals(col1, Ans.getCol(arr2, 1));
+        assert Arrays.equals(col1, Problems.getCol(arr2, 1));
         col1[0] =-2;col1[1]=-5;col1[2]=-8;
-        assert Arrays.equals(col1, Ans.getCol(arr3,1));
+        assert Arrays.equals(col1, Problems.getCol(arr3,1));
 
         int[] row2 = {7,8,9,7};
-        assert Arrays.equals(row2,Ans.getRow(arr2, 2));
+        assert Arrays.equals(row2,Problems.getRow(arr2, 2));
         int[] row22 = {-8,-8,-8};
-        assert Arrays.equals(row22,Ans.getRow(arr3, 2));
-        assert Ans.signChangeCount(arr3) == 0;
-        assert Ans.signChangeCount(arr4) == 5;
-        assert Ans.signChangeCount2(arr2) == 0;
-        assert Ans.signChangeCount2(arr4) == 5;
+        assert Arrays.equals(row22,Problems.getRow(arr3, 2));
+        assert Problems.signChangeCount(arr3) == 0;
+        assert Problems.signChangeCount(arr4) == 5;
+        assert Problems.signChangeCount2(arr2) == 0;
+        assert Problems.signChangeCount2(arr4) == 5;
         int[] mCols = {7,8,9};
-        assert Arrays.equals(mCols,Ans.maxCols(arr1));
+        assert Arrays.equals(mCols,Problems.maxCols(arr1));
         int[] mCols2 = {-1,-2,-3};
-        assert Arrays.equals(mCols2,Ans.maxCols(arr3));
+        assert Arrays.equals(mCols2,Problems.maxCols(arr3));
         double[] aRow = {2.75,5.25,7.75};
-        assert Arrays.equals(aRow, Ans.avgRows(arr2));
-        assert Ans.minAvgRows(arr3) == 2;
-        assert Ans.colWithRepetitions(arr4, 0) == 2;
-        assert Ans.oddRowsAndCols(arr2)==4;
-        assert Ans.oddRowsAndCols(arr3) == 0;
-        assert Ans.oddRowsAndCols(arr4) == 1;
-        assert Ans.countUntilNegative(arr2) == -1;
+        assert Arrays.equals(aRow, Problems.avgRows(arr2));
+        assert Problems.minAvgRows(arr3) == 2;
+        assert Problems.colWithRepetitions(arr4, 0) == 2;
+        assert Problems.oddRowsAndCols(arr2)==4;
+        assert Problems.oddRowsAndCols(arr3) == 0;
+        assert Problems.oddRowsAndCols(arr4) == 1;
+        assert Problems.countUntilNegative(arr2) == -1;
 
         ParkingSpot[][] spots = new ParkingSpot[3][3];
         spots[0][0] = new ParkingSpot(4, 10); spots[0][0].setOccupied();
@@ -61,13 +61,13 @@ public class Test {
         spots[2][1] = new ParkingSpot(8,92);
         spots[2][2] = new ParkingSpot(66,94);
 
-        assert Ans.findAParkingSpot(spots).equals(spots[0][2]);
-        assert Ans.findConsecutiveSpots(spots).equals(spots[1][0]);
+        assert Problems.findAParkingSpot(spots).equals(spots[0][2]);
+        assert Problems.findConsecutiveSpots(spots).equals(spots[1][0]);
         int[]  spotNums ={17,9,66};
-        Ans.reserveSpots(spots,spotNums);
+        Problems.reserveSpots(spots,spotNums);
         assert spots[0][1].isOccupied() && spots[1][2].isOccupied() && spots[2][2].isOccupied();
         spots[0][2].setOccupied();
-        assert Ans.bestSpot(spots).equals(spots[1][0]);
+        assert Problems.bestSpot(spots).equals(spots[1][0]);
         System.out.println("All Tests Passed");
          
     }
